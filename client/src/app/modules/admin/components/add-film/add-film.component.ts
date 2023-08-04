9138b0a95bf6c6c5370d75e0649895bdfaf5cd46
ragudeepak <124037558+ragudeepak@users.noreply.github.com>
2023-08-04 17:57:13 +0530

    inital commit

---
+++ client/src/app/modules/admin/components/add-film/add-film.component.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-film',
+  templateUrl: './add-film.component.html',
+  styleUrls: ['./add-film.component.css']
+})
+export class AddFilmComponent {
+  formData: any = {}; // Declare an object to store form data
+  theatreFormData: any = {};
+
+  constructor(private http:HttpClient){}
+
+  onSubmit() {
+    // Handle form submission logic
+    console.log(this.formData); // Example: Log the form data
+    alert('Film Added.')
+    this.http.post('http://localhost:5100/movies',this.formData).subscribe((res) => {
+      console.log(res)
+      alert('Film Added.')
+    })
+  }
+
+  onSubmitTheatreData(){
+    console.log(this.theatreFormData); // Example: Log the form data
+    this.http.post('http://localhost:5100/theaters',this.theatreFormData).subscribe((res) => {
+      console.log(res)
+      alert('Thetre Added.')
+    })
+  }
+}
